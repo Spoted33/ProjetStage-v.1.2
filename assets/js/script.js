db2ec994045ffd6e71191eb711c90721593441eb
Spoted33 <yannprice33@gmail.com>
2024-06-24 13:08:37 +0200

    debugJS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,23 @@ fetch('footer.html') // créer une requête pour récupérer le contenu du fichi
 .then(response => response.text()) // convertir la réponse en texte
 .then(data => { // une fois que le texte est récupéré
   document.getElementById('footer').innerHTML = data; // insérer le texte dans la div au dessus ;-)
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.getElementById('footer').classList.add('dark-mode');
+  }
+
+  const toggleButton = document.getElementById('toggle-dark-mode');
+  if (toggleButton) {
+    toggleButton.addEventListener('click', function() {
+      document.getElementById('footer').classList.toggle('dark-mode');
+
+  if (document.getElementById('footer').classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+})};
 });
